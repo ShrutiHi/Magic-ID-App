d04177e4b3d122b822efbef36c7055a70da26e16
Shrutika Hilale <shrutikahilale8@gmail.com>
2022-08-04 10:28:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/magicidapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.magicidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etID;
+    Button btnSubmit;
+    TextView tvResults;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etID = findViewById(R.id.etID);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        tvResults = findViewById(R.id.tvResults);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                trim will delete any whitespaces leading and trailing
+                String idNumber = etID.getText().toString().trim();
+                String dob = idNumber.substring(0,6);
+
+//                String gender = idNumber.substring(6,10);
+                int gender = Integer.parseInt(Character.toString(idNumber.charAt(6)));
+                String sGender;
+                if(gender < 5){
+                    sGender = getString(R.string.female);
+                }
+                else{
+                    sGender = getString(R.string.male);
+                }
+
+//                String citizenship = idNumber.substring(10,11);
+                int citizenship = Integer.parseInt(Character.toString(idNumber.charAt(10)));
+                String sCitizenship;
+                if(citizenship == 1){
+                    sCitizenship = getString(R.string.pr);
+                }
+                else{
+                    sCitizenship = getString(R.string.sac);
+                }
+
+                String txt = getString(R.string.dob)+ dob+ getString(R.string.newline)+getString(R.string.gender)+sGender + getString(R.string.newline)+ getString(R.string.citizenship)+sCitizenship;
+
+                tvResults.setText(txt);
+            }
+        });
+
+    }
+}
