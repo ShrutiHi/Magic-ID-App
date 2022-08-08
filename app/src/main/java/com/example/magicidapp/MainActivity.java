0b0cd861026576554d25513502cd2d3ed5776b58
Shrutika Hilale <shrutikahilale8@gmail.com>
2022-08-08 22:18:08 +0530

    Added code to MainActivity.java to manage errors.

--- app/src/main/java/com/example/magicidapp/MainActivity.java
+++ app/src/main/java/com/example/magicidapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText etID;
@@ -25,33 +26,52 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                trim will delete any whitespaces leading and trailing
-                String idNumber = etID.getText().toString().trim();
-                String dob = idNumber.substring(0,6);
+//
+                if(etID.getText().toString().isEmpty() || etID.getText().toString().length() != 13){
+                    Toast.makeText(MainActivity.this, "Please enter valid id number", Toast.LENGTH_SHORT).show();
+                }
+                else{
+//                    trim will delete any whitespaces leading and trailing
+                    String idNumber =  etID.getText().toString().trim();
+
+                    int month = Integer.parseInt(idNumber.substring(2,4));
+                    int day = Integer.parseInt(idNumber.substring(4,6));
+                    int nationality = Integer.parseInt(idNumber.substring(10,11));
+
+                    if((month>12) || (day>31) || !(nationality==0 || nationality==1)) {
+                        Toast.makeText(MainActivity.this, "Please enter valid id number", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        String dob = idNumber.substring(4,6) + "/" + idNumber.substring(2,4)+"/"+idNumber.substring(0,2);
 
 //                String gender = idNumber.substring(6,10);
-                int gender = Integer.parseInt(Character.toString(idNumber.charAt(6)));
-                String sGender;
-                if(gender < 5){
-                    sGender = getString(R.string.female);
-                }
-                else{
-                    sGender = getString(R.string.male);
-                }
+                        int gender = Integer.parseInt(Character.toString(idNumber.charAt(6)));
+                        String sGender;
+                        if(gender < 5){
+                            sGender = getString(R.string.female);
+                        }
+                        else{
+                            sGender = getString(R.string.male);
+                        }
 
 //                String citizenship = idNumber.substring(10,11);
-                int citizenship = Integer.parseInt(Character.toString(idNumber.charAt(10)));
-                String sCitizenship;
-                if(citizenship == 1){
-                    sCitizenship = getString(R.string.pr);
-                }
-                else{
-                    sCitizenship = getString(R.string.sac);
+                        int citizenship = Integer.parseInt(Character.toString(idNumber.charAt(10)));
+                        String sCitizenship;
+                        if(citizenship == 1){
+                            sCitizenship = getString(R.string.pr);
+                        }
+                        else{
+                            sCitizenship = getString(R.string.sac);
+                        }
+
+                        String txt = getString(R.string.dob)+ dob+ getString(R.string.newline)+getString(R.string.gender)+sGender + getString(R.string.newline)+ getString(R.string.citizenship)+sCitizenship;
+
+                        tvResults.setText(txt);
+                    }
+
                 }
 
-                String txt = getString(R.string.dob)+ dob+ getString(R.string.newline)+getString(R.string.gender)+sGender + getString(R.string.newline)+ getString(R.string.citizenship)+sCitizenship;
 
-                tvResults.setText(txt);
             }
         });
 
